2cb18d20ef60979092f6724768c37658a14852d0
Masahiro Aikawa <aikawame@wor.jp>
2021-10-20 12:33:22 +0900

    :sparkles: Add notification area

---
+++ NotificationArea.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Naorai
+{
+  public partial class NotificationArea
+  {
+    private void Edit_Click(object sender, RoutedEventArgs e)
+    {
+      Window mainWindow = new MainWindow();
+      mainWindow.Show();
+    }
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+      Application.Current.Shutdown();
+    }
+  }
+}
