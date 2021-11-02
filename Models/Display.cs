a5c9656e47d04347e0a7e444cd4d55dcbabaf3b8
Masahiro Aikawa <aikawame@wor.jp>
2021-11-02 13:51:09 +0900

    :sparkles: Add function to restore windows

---
+++ Models/Display.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace Naorai.Models
+{
+  public class Display
+  {
+    public ObservableCollection<Window> Windows = new ObservableCollection<Window>();
+
+    private string GetSettingFileName()
+    {
+      string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+      double width = SystemParameters.VirtualScreenWidth;
+      double height = SystemParameters.VirtualScreenHeight;
+
+      return $"{appDataPath}/Naorai/Settings_{width}x{height}.xml";
+    }
+
+    public void Load()
+    {
+      WindowManager windowManager = new WindowManager();
+      ObservableCollection<ActiveWindow> activeWindows = windowManager.GetActiveWindows();
+
+      XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Window>));
+      StreamReader streamReader = new StreamReader(GetSettingFileName(), new UTF8Encoding(false));
+      ObservableCollection<Window> windows = (ObservableCollection<Window>)serializer.Deserialize(streamReader);
+      streamReader.Close();
+
+      foreach (ActiveWindow activeWindow in activeWindows)
+      {
+        IEnumerable<Window> windowsWhere = windows.Where(o =>
+        {
+          return o.ProcessName == activeWindow.ProcessName && o.Title == activeWindow.Title;
+        });
+        foreach (Window window in windowsWhere)
+        {
+          Debug.Print(window.Title);
+          NativeMethods.MoveWindow(
+            activeWindow.Handler,
+            (int)window.Rect.X,
+            (int)window.Rect.Y - (int)window.Rect.Height,
+            (int)window.Rect.Width,
+            (int)window.Rect.Height,
+            false
+          );
+        }
+      }
+    }
+
+    public void Save()
+    {
+      WindowManager windowManager = new WindowManager();
+      ObservableCollection<Window> windows = windowManager.GetWindows();
+
+      XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Window>));
+      StreamWriter streamWriter = new StreamWriter(GetSettingFileName(), false, new UTF8Encoding(false));
+      serializer.Serialize(streamWriter, windows);
+      streamWriter.Close();
+    }
+
+  }
+}
---
+++ Models/WindowManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Naorai.Models
+{
+  public class WindowManager
+  {
+    private ObservableCollection<ActiveWindow> CurrentWindows = new ObservableCollection<ActiveWindow>();
+
+    private ObservableCollection<Window> Windows = new ObservableCollection<Window>();
+
+    private static readonly string[] IgnoredProcessNames = { "Naorai", "ApplicationFrameHost", "SystemSettings" };
+
+    public WindowManager()
+    {
+      NativeMethods.EnumWindows(EnumWindowProcess, IntPtr.Zero);
+    }
+
+    public ObservableCollection<ActiveWindow> GetActiveWindows()
+    {
+      return CurrentWindows;
+    }
+
+    public ObservableCollection<Window> GetWindows()
+    {
+      return Windows;
+    }
+
+    private static string GetTitle(IntPtr hWnd)
+    {
+      if (NativeMethods.IsWindowVisible(hWnd) == false) return "";
+
+      int textLength = NativeMethods.GetWindowTextLength(hWnd);
+      if (textLength == 0) return "";
+
+      StringBuilder title = new StringBuilder(textLength + 1);
+      NativeMethods.GetWindowText(hWnd, title, title.Capacity);
+
+      return title.ToString();
+    }
+
+    private static Process GetProcess(IntPtr hWnd)
+    {
+      uint processId;
+      NativeMethods.GetWindowThreadProcessId(hWnd, out processId);
+
+      return Process.GetProcessById((int)processId);
+    }
+
+    private static Rect GetRect(IntPtr hWnd)
+    {
+      NativeMethods.Rect win32Rect;
+      NativeMethods.GetWindowRect(hWnd, out win32Rect);
+
+      Rect rect = new Rect();
+      rect.Width = win32Rect.right - win32Rect.left;
+      rect.Height = win32Rect.bottom - win32Rect.top;
+      rect.X = win32Rect.left;
+      rect.Y = win32Rect.bottom;
+
+      return rect;
+    }
+
+    private bool EnumWindowProcess(IntPtr hWnd, IntPtr lParam)
+    {
+      string title = GetTitle(hWnd);
+      if (title == "") return true;
+
+      Process process = GetProcess(hWnd);
+      if (IgnoredProcessNames.Contains(process.ProcessName)) return true;
+
+      Rect rect = GetRect(hWnd);
+
+      ActiveWindow activeWindow = new ActiveWindow();
+      activeWindow.ProcessName = process.ProcessName;
+      activeWindow.Title = title;
+      activeWindow.Rect = rect;
+      activeWindow.Handler = hWnd;
+      CurrentWindows.Add(activeWindow);
+
+      Window window = new Window();
+      window.ProcessName = process.ProcessName;
+      window.Title = title;
+      window.Rect = rect;
+      Windows.Add(window);
+
+      return true;
+    }
+  }
+}
