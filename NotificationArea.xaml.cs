e9d74075bf5a8eac0f0a4afb1080d0b8411acf7e
Masahiro Aikawa <aikawame@wor.jp>
2021-11-02 13:51:09 +0900

    Add function to restore windows

--- NotificationArea.xaml.cs
+++ NotificationArea.xaml.cs
@@ -1,12 +1,26 @@
 ﻿using System.Windows;
+using Naorai.Models;
 
 namespace Naorai
 {
   public partial class NotificationArea
   {
+    private void Restore_Click(object sender, RoutedEventArgs e)
+    {
+      Display display = new Display();
+      display.Load();
+    }
+
+    private void StoreAll_Click(object sender, RoutedEventArgs e)
+    {
+      Display display = new Display();
+      display.Save();
+      MessageBox.Show("All window positions have been saved.");
+    }
+
     private void Edit_Click(object sender, RoutedEventArgs e)
     {
-      Window mainWindow = new MainWindow();
+      System.Windows.Window mainWindow = new MainWindow();
       mainWindow.Show();
     }
     private void Exit_Click(object sender, RoutedEventArgs e)
