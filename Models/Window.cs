056da651c384c8edecc74ceb805e1f5a455d3155
Masahiro Aikawa <aikawame@wor.jp>
2021-11-04 18:05:50 +0900

    :art: Change setting file type to JSON

--- Models/Window.cs
+++ Models/Window.cs
@@ -1,14 +1,22 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.Serialization;
+using System.Windows;
 
 namespace Naorai.Models
 {
-   public class Window
+  [DataContract]
+  public class Window
   {
+    [DataMember]
     public string ProcessName { get; set; }
 
+    [DataMember]
     public string Title { get; set; }
 
+    [DataMember]
     public Rect Rect { get; set; }
+
+    public IntPtr Handler { get; set; }
 
     public Window()
     {
