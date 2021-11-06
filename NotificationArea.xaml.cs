5b06b111f738f9c3a484fba042bdb5514c9abd56
Masahiro Aikawa <aikawame@wor.jp>
2021-11-06 18:11:31 +0900

    Refactor models

--- NotificationArea.xaml.cs
+++ NotificationArea.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using Naorai.Models;
+using Window = Naorai.Models.Window;
 
 namespace Naorai
 {
@@ -7,15 +8,26 @@ namespace Naorai
   {
     private void Restore_Click(object sender, RoutedEventArgs e)
     {
-      Display display = new Display();
-      display.Load();
+      SettingCollection settingCollection = SettingCollection.Load();
+      settingCollection.GetCurrentScreen().Apply();
     }
 
     private void StoreAll_Click(object sender, RoutedEventArgs e)
     {
-      Display display = new Display();
-      display.Save();
+      SettingCollection settingCollection = SettingCollection.Load();
+      settingCollection.UpdateScreen(Screen.Active());
+      settingCollection.Save();
+
       MessageBox.Show("All window positions have been saved.");
+    }
+
+    private void StoreActive_Click(object sender, RoutedEventArgs e)
+    {
+      SettingCollection settingCollection = SettingCollection.Load();
+      settingCollection.GetCurrentScreen().UpdateWindow(Window.Active());
+      settingCollection.Save();
+
+      MessageBox.Show("Active window position has been saved.");
     }
 
     private void Edit_Click(object sender, RoutedEventArgs e)
