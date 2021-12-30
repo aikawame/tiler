5790480ee75734258d53bb381de0e6a44956e4f1
Masahiro Aikawa <aikawame@wor.jp>
2021-12-31 00:15:36 +0900

    Fix for nullable and add disposer

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,12 +5,22 @@ namespace Tiler
 {
   public partial class App
   {
-    private TaskbarIcon _taskbarIcon;
+    private TaskbarIcon? _taskbarIcon;
 
     protected override void OnStartup(StartupEventArgs e)
     {
-      _taskbarIcon = (TaskbarIcon)FindResource("TaskbarIcon");
       base.OnStartup(e);
+      var taskbarIcon = FindResource("TaskbarIcon");
+      if (taskbarIcon is null) return;
+
+      _taskbarIcon = (TaskbarIcon)taskbarIcon;
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+      if (_taskbarIcon is not null) _taskbarIcon.Dispose();
+
+      base.OnExit(e);
     }
   }
 }
