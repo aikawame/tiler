27b5e76a1b606659b95ed3fe4d56b1deee1fe398
Masahiro Aikawa <aikawame@wor.jp>
2022-01-03 19:33:49 +0900

    :adhesive_bandage: Enable detection of display resolution changes

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Hardcodet.Wpf.TaskbarNotification;
+using Microsoft.Win32;
+using Tiler.Models;
 
 namespace Tiler;
 
 public partial class App
 {
+  public App()
+  {
+    SystemEvents.DisplaySettingsChanged += SystemEvents_DisplaySettingsChanged;
+  }
+
   private TaskbarIcon? _taskbarIcon;
 
   protected override void OnStartup(StartupEventArgs e)
@@ -21,5 +29,10 @@ public partial class App
     if (_taskbarIcon is not null) _taskbarIcon.Dispose();
 
     base.OnExit(e);
+  }
+
+  private void SystemEvents_DisplaySettingsChanged(object? sender, EventArgs e)
+  {
+    SettingCollection.Load().GetCurrentScreen().Apply();
   }
 }
