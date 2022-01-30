2e5eee742b2271f444f9c3a3ddff79827fb79eba
Masahiro Aikawa <aikawame@wor.jp>
2022-01-30 22:19:47 +0900

    :sparkles: Make resolution tabs sortable

--- Tiler/Models/SettingCollection.cs
+++ Tiler/Models/SettingCollection.cs
@@ -69,7 +69,21 @@ public class SettingCollection
 
   public List<Screen> GetScreens()
   {
-    return Screens;
+    var screenWidth = (int)SystemParameters.VirtualScreenWidth;
+    var screenHeight = (int)SystemParameters.VirtualScreenHeight;
+
+    var screens = new List<Screen>();
+    foreach (var s in Screens)
+    {
+      if (s.Width == screenWidth && s.Height == screenHeight)
+      {
+        screens.Insert(0, s);
+        continue;
+      }
+      screens.Add(s);
+    }
+
+    return screens;
   }
 
   public void Save()
